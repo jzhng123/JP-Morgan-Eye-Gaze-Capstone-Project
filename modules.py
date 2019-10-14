c0ace315d4f419e6299d761d733f2b4b51000b66
litchiyang <yanglinxiuzhi@hotmail.com>
2019-10-14 00:06:48 -0400

    Sudoku game 1.0

---
+++ modules.py
@@ -0,0 +1,117 @@
+import copy
+import random
+import pygame
+
+LENGTH = 1080
+WIDTH = 720
+BLACK = [0,0,0]
+
+class Card(pygame.sprite.Sprite):
+	def __init__(self, x, y, width, height, text, font, font_colors, bg_colors, attribute, **kwargs):
+		pygame.sprite.Sprite.__init__(self)
+		self.rect = pygame.Rect(x, y, width, height)
+		self.text = text
+		self.attribute = attribute
+		self.font_info = font
+		self.font = pygame.font.SysFont("comicsansms", font)
+		self.font_colors = font_colors
+		self.is_selected = False
+		self.select_order = None
+		self.bg_colors = bg_colors
+	def draw(self, screen, mouse_pos):
+		pygame.draw.rect(screen, self.bg_colors[1], self.rect, 0)
+
+		pygame.draw.rect(screen, BLACK, [LENGTH/12, WIDTH*25/108, LENGTH*20/36, 3])
+		pygame.draw.rect(screen, BLACK, [LENGTH/12, WIDTH*40/108, LENGTH*20/36, 10])
+		pygame.draw.rect(screen, BLACK, [LENGTH/12, WIDTH*55/108, LENGTH*20/36, 3])
+
+		pygame.draw.rect(screen, BLACK, [LENGTH*8/36, WIDTH*5/54, 3, WIDTH*20/36])
+		pygame.draw.rect(screen, BLACK, [LENGTH*13/36, WIDTH*5/54, 10, WIDTH*20/36])
+		pygame.draw.rect(screen, BLACK, [LENGTH*18/36, WIDTH*5/54, 3, WIDTH*20/36])
+
+		pygame.draw.rect(screen, BLACK, [LENGTH*11/12, WIDTH*25/108, LENGTH*5/36, 3])
+		pygame.draw.rect(screen, BLACK, [LENGTH*11/12, WIDTH*40/108, LENGTH*5/36, 3])
+		pygame.draw.rect(screen, BLACK, [LENGTH*11/12, WIDTH*55/108, LENGTH*5/36, 3])
+
+		if self.text == "?":
+			pygame.draw.rect(screen, self.bg_colors[2], self.rect, 0)
+		if self.rect.collidepoint(mouse_pos):
+			pygame.draw.rect(screen, self.bg_colors[0], self.rect, 0)
+
+		font_color = self.font_colors[self.is_selected]
+		text_render = self.font.render(self.text, True, font_color)
+		font_size = self.font.size(self.text)
+		screen.blit(text_render, (self.rect.x+(self.rect.width-font_size[0])/2,
+								  self.rect.y+(self.rect.height-font_size[1])/2))
+
+
+
+class Button(Card):
+	def __init__(self, x, y, width, height, text, font, font_colors, bg_colors, attribute, **kwargs):
+		Card.__init__(self, x, y, width, height, text, font, font_colors, bg_colors, attribute)
+
+	def do(self, game_gen, func, sprites_group, objs):
+		if self.attribute == 'NEXT':
+			for obj in objs:
+				obj.font = pygame.font.SysFont("comicsansms", obj.font_info)
+				obj.text = obj.attribute
+			self.font = pygame.font.SysFont("comicsansms", self.font_info)
+			self.text = self.attribute
+			game_gen.generate(True)
+			sprites_group = func(game_gen.puzzle)
+
+		elif self.attribute == 'RESET':
+			for obj in objs:
+				obj.font = pygame.font.SysFont("comicsansms", obj.font_info)
+				obj.text = obj.attribute
+
+			game_gen.puzzle = game_gen.puzzle_ori
+			sprites_group = func(game_gen.puzzle)
+
+		return sprites_group
+
+
+class gameGenerator():
+	def __init__(self):
+		self.level = 0
+		self.info = 'gameGenerator'
+	def generate(self, next_level = False):
+		self.__reset()
+
+		if next_level:
+			self.level += 1
+		
+		self.puzzle_ori = self.__game(self.level)
+		self.puzzle = copy.deepcopy(self.puzzle_ori)
+
+	def __reset(self):
+		self.puzzle = []
+		self.puzzle_ori = []
+
+	def __game(self, level):
+
+		#easy
+		if level == 0:
+			self.puzzle = [1, "?", 3, 4, 4, 3, 2, 1, 2, 4, "?", 3, 3, 1, 4, 2]
+		elif level == 1:
+			self.puzzle = [2, 4, "?", 3, 3, 1, 2, 4, 1, 3, 4, "?", 4, 2, 3, 1]
+		elif level == 2:
+			self.puzzle = ["?", 2, 4, 1, 4, 1, 3, 2, 2, 4, "?", 3, 1, 3, 2, 4]
+		
+		#medium
+		elif level == 3:
+			self.puzzle = ["?", 3, 1, "?", 1, "?", "?", 3, 2, "?", 3, 4, "?", 4, 2, "?"]
+		elif level == 4:
+			self.puzzle = [3, "?", 1, "?", "?", 1, "?", 3, "?", 3, 4, "?", 1, 4, "?", 2]
+		elif level == 5:
+			self.puzzle = [1, "?", 2, 3, "?", 3, "?", 4, 3, "?", 4, "?", "?", 1, "?", 2]
+
+		#hard
+		elif level == 6:
+			self.puzzle = [2, "?", "?", "?", "?", "?", 4, "?", "?", 3, "?", "?", "?", "?", "?", 3]
+		elif level == 7:
+			self.puzzle = ["?", 4, "?", "?", "?", "?", 3, "?", "?", 2, "?", "?", 1, "?", "?", "?"]
+		elif level == 8:
+			self.puzzle = ["?", "?", "?", 3, "?", 3, 4, "?", "?", "?", 2, "?", "?", 2, "?", "?"]
+		
+		return self.puzzle
---
+++ sudoku.py
@@ -0,0 +1,212 @@
+import os
+import sys
+import pygame
+from modules import *
+from fractions import Fraction
+
+
+'''Constants'''
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+PURPLE = (153, 0, 153)
+LIGHT_PURPLE = (255, 204, 255)
+YELLOW = (255, 255, 0)
+ORANGE = (255, 128, 0)
+GREEN = (153, 0, 0)
+PALETURQUOISE = (175, 238, 238)
+AZURE = (240, 255, 255)
+
+# CURRENTPATH = os.path.abspath(os.path.dirname(__file__))
+# FONTPATH = os.path.join(CURRENTPATH, 'fonts/font.TTF')
+LENGTH = 1080
+WIDTH = 720
+SCREENSIZE = (LENGTH, WIDTH)
+GROUPTYPES = ['BLANK', 'NUMBER', 'BUTTON']
+
+
+'''The Puzzle'''
+PUZZLEFONT_COLORS = [BLACK, YELLOW]
+PUZZLE_COLORS = [PALETURQUOISE, PURPLE, LIGHT_PURPLE]
+PUZZLEFONT = 50
+PUZZLE_POSITIONS = [(LENGTH/12, WIDTH*5/54, LENGTH*5/36, WIDTH*5/36), (LENGTH*8/36, WIDTH*5/54, LENGTH*5/36, WIDTH*5/36), (LENGTH*13/36, WIDTH*5/54, LENGTH*5/36, WIDTH*5/36),(LENGTH*18/36, WIDTH*5/54, LENGTH*5/36, WIDTH*5/36),
+						(LENGTH/12, WIDTH*25/108, LENGTH*5/36, WIDTH*5/36), (LENGTH*8/36, WIDTH*25/108, LENGTH*5/36, WIDTH*5/36), (LENGTH*13/36, WIDTH*25/108, LENGTH*5/36, WIDTH*5/36), (LENGTH*18/36, WIDTH*25/108, LENGTH*5/36, WIDTH*5/36),
+						(LENGTH/12, WIDTH*40/108, LENGTH*5/36, WIDTH*5/36), (LENGTH*8/36, WIDTH*40/108, LENGTH*5/36, WIDTH*5/36), (LENGTH*13/36, WIDTH*40/108, LENGTH*5/36, WIDTH*5/36), (LENGTH*18/36, WIDTH*40/108, LENGTH*5/36, WIDTH*5/36),
+						(LENGTH/12, WIDTH*55/108, LENGTH*5/36, WIDTH*5/36), (LENGTH*8/36, WIDTH*55/108, LENGTH*5/36, WIDTH*5/36), (LENGTH*13/36, WIDTH*55/108, LENGTH*5/36, WIDTH*5/36), (LENGTH*18/36, WIDTH*55/108, LENGTH*5/36, WIDTH*5/36)]
+
+'''The Options'''
+OPTIONS = [1, 2, 3, 4]
+OPTIONSFONT_COLORS = [BLACK, GREEN]
+OPTIONS_COLORS = [PALETURQUOISE, YELLOW]
+OPTIONSFONT = 50
+OPTIONSCARD_POSITIONS = [(LENGTH*11/12, WIDTH*5/54, LENGTH*5/36, WIDTH*5/36), (LENGTH*11/12, WIDTH*25/108, LENGTH*5/36, WIDTH*5/36), (LENGTH*11/12, WIDTH*40/108, LENGTH*5/36, WIDTH*5/36), (LENGTH*11/12, WIDTH*55/108, LENGTH*5/36, WIDTH*5/36)]
+
+'''Buttons'''
+BUTTONS = ['RESET', 'NEXT']
+BUTTONFONT_COLORS = [BLACK, BLACK]
+BUTTONCARD_COLORS = [PALETURQUOISE, ORANGE]
+BUTTONFONT = 30
+BUTTONCARD_POSITIONS = [(LENGTH/12, WIDTH*7/8, LENGTH*5/36, WIDTH*5/36), (LENGTH*11/12, WIDTH*7/8, LENGTH*5/36, WIDTH*5/36)]
+
+
+
+'''Find the selected buttons'''
+def buttonSelected(group, mouse, group_type, blanks = None):
+	selected = []
+
+	if group_type == GROUPTYPES[0]:
+		for i, each in enumerate(group):
+			x = int(mouse[0])
+			y = int(mouse[1])
+			if x >= LENGTH/12 and x <= LENGTH*23/36 and y >= WIDTH*5/54 and y <= WIDTH*60/108:
+				if each.is_selected:
+					each.is_selected = not each.is_selected
+			if each.rect.collidepoint(mouse) and (i in blanks):
+				each.is_selected = not each.is_selected
+			if each.is_selected:
+				selected.append(i)
+	
+	elif group_type == GROUPTYPES[1]:
+		for i, each in enumerate(group):
+			if each.is_selected:
+				each.is_selected = not each.is_selected
+			if each.rect.collidepoint(mouse):
+				each.is_selected = not each.is_selected
+			if each.is_selected:
+				selected.append(each.attribute)
+
+	elif group_type == GROUPTYPES[2]:
+		for each in group:
+			if each.rect.collidepoint(mouse):
+				each.is_selected = True
+				selected.append(each.attribute)
+
+	return selected
+
+
+'''Get group of puzzle'''
+def getPuzzleGroup(numbers):
+	puzzle_group = pygame.sprite.Group()
+	for i, number in enumerate(numbers):
+		args = (*PUZZLE_POSITIONS[i], str(number), PUZZLEFONT, PUZZLEFONT_COLORS, PUZZLE_COLORS, str(number))
+		puzzle_group.add(Card(*args))
+	return puzzle_group
+
+
+'''Get group of options'''
+def getOptionGroup(options):
+	option_group = pygame.sprite.Group()
+	for i, option in enumerate(options):
+		args = (*OPTIONSCARD_POSITIONS[i], str(option), OPTIONSFONT, OPTIONSFONT_COLORS, OPTIONS_COLORS, str(option))
+		option_group.add(Card(*args))
+	return option_group
+
+
+'''Get group of buttons'''
+def getButtonGroup(buttons):
+	button_group = pygame.sprite.Group()
+	for i, button in enumerate(buttons):
+		args = (*BUTTONCARD_POSITIONS[i], str(button), BUTTONFONT, BUTTONFONT_COLORS, BUTTONCARD_COLORS, str(button))
+		button_group.add(Button(*args))
+	return button_group
+
+
+'''Show information on screen'''
+def showInfo(text, screen, w, h):
+	pygame.draw.rect(screen, WHITE, (0, 0, h, w))
+	font = pygame.font.SysFont('Consolas', 100)
+	screen.blit(font.render(text, True, (0, 0, 0)), ((h-font.size(text)[0])/2, (w-font.size(text)[1])/2))
+
+'''main function of the game'''
+def main():
+	pygame.init()
+	pygame.mixer.init()
+
+	#Get size of screen
+	info = pygame.display.Info()
+	width = info.current_w
+	height = info.current_h
+	SCREENSIZE = (width, height)
+
+	screen = pygame.display.set_mode(SCREENSIZE)
+
+	game_gen = gameGenerator()
+	game_gen.generate()
+
+	puzzle_group = getPuzzleGroup(game_gen.puzzle)
+	option_group = getOptionGroup(OPTIONS)
+	button_group = getButtonGroup(BUTTONS)
+
+	clock = pygame.time.Clock()
+
+	counter = 50
+
+	selected_blank = []
+	selected_number = []
+	selected_button = []
+
+	blanks = []
+	for i in range(len(game_gen.puzzle)):
+		if game_gen.puzzle[i] == "?":
+			blanks.append(i)
+
+	finished = False
+	while True:
+
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				sys.exit(-1)
+			elif event.type == pygame.MOUSEBUTTONUP:
+				mouse_pos = pygame.mouse.get_pos()
+				selected_blank = buttonSelected(puzzle_group, mouse_pos, 'BLANK', blanks)
+				selected_number = buttonSelected(option_group, mouse_pos, 'NUMBER')
+				selected_button = buttonSelected(button_group, mouse_pos, 'BUTTON')
+	
+		screen.fill((255, 255, 255))
+
+		if len(selected_number) == 1 and len(selected_blank) == 1:
+			game_gen.puzzle[selected_blank[0]] = int(selected_number[0])
+			print(game_gen.puzzle)
+
+			selected_number = []
+			selected_blank = []
+			puzzle_group = getPuzzleGroup(game_gen.puzzle)
+
+
+		for each in puzzle_group:
+			each.draw(screen, pygame.mouse.get_pos())
+		for each in option_group:
+			each.draw(screen, pygame.mouse.get_pos())
+
+		for each in button_group:
+			if selected_button and selected_button[0] == "NEXT":
+				counter = 30
+				print(game_gen.level)
+				if game_gen.level == 8:
+					finished = True
+
+			if selected_button and each.attribute == selected_button[0]:
+				each.is_selected = False
+				puzzle_group = each.do(game_gen, getPuzzleGroup, puzzle_group, button_group)
+				selected_button = []
+
+				blanks = []
+				for i in range(len(game_gen.puzzle)):
+					if game_gen.puzzle[i] == "?":
+						blanks.append(i)
+			each.draw(screen, pygame.mouse.get_pos())
+
+		if counter >= 0:
+			if not finished:
+				counter -= 1
+				showInfo("Ready? Start!", screen, height, width)
+			else:
+				showInfo("Congrats! You've finished the game", screen, height, width)
+	
+
+		pygame.display.flip()
+		clock.tick(30)
+
+
+if __name__ == '__main__':
+	main()
